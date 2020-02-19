5745b03c270f6ff912fdd59c97e06f509561bc98
jessesbyers <jesse.s.byers@gmail.com>
2020-02-19 15:23:39 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  document.querySelectorAll("input")[1].addEventListener("click", function(event) {
+    event.preventDefault();
+
+    let newTask = document.getElementById("new-task-description").value;
+    let element = document.getElementById("tasks");
+    let li = document.createElement('li');
+
+    let button = document.createElement('button');
+      button.innerHTML = "X"
+
+    li.innerHTML = newTask;
+    element.appendChild(li).appendChild(button);
+
+    document.getElementById("new-task-description").value = ""
+
+    // document.querySelector("li button").addEventListener("click", function(event) {
+    //   let task = document.querySelector("li").innerText;
+    //   li.remove(task);
+    // });
+  })
 });
