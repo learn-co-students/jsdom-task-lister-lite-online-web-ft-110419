dee86f1ee9efe19a4b93d30a6fc02b471205fe74
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-02-20 14:26:35 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
+
+  document.getElementsByTagName("input")[1].addEventListener("click", function(event) {
+    event.preventDefault();
+
+  let newTask = document.getElementById('new-task-description').value;
+  let element = document.getElementById("tasks");
+  let listItem = document.createElement("li");
+
+  let button = document.createElement('button');
+    button.innerText = "DEL";
+
+  listItem.innerHTML = newTask;
+  element.appendChild(listItem).appendChild(button);
+
+  document.getElementById("new-task-desciption").value = ""
+
+  })
+
+});
+
+
+/* document.addEventListener("DOMContentLoaded", () => {
   document.getElementsByTagName("input")[1].addEventListener("click", function(event){
     event.preventDefault();
   }, false);
@@ -14,8 +33,38 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
 
-/* document.querySelector("#id-checkbox").addEventListener("click", function(event) {
+ document.querySelector("#id-checkbox").addEventListener("click", function(event) {
   document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
   event.preventDefault();
-}, false); 
+}, false); s
+*/
+
+
+/*
+  // grab necessary DOM elements
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+
+  // where to send tasks
+  const tasksUl = document.getElementById("tasks");
+
+  // event listener
+  const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
+
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    taskList.createNewTask(newTaskDescription.value);
+    e.target.reset();
+    renderApp();
+  });
+
+  taskUl.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON"){
+      taskList.deleteTask(e.target.dataset.description);
+      renderApp();
+    }
+  });
+
+});
 */
