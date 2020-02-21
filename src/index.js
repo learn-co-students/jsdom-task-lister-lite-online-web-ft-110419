71495eb40b6cd51986cc277d5ebc5a7ba1830f68
Dominique Skaf <doskaf@yahoo.com>
2020-02-21 19:26:57 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let newTaskBox = document.getElementById("new-task-description");
+let submitButton = document.getElementsByTagName("input")[1];
+
+submitButton.addEventListener("click", function(event) {
+  let node = document.createElement("LI");
+  let textnode = document.createTextNode(`${newTaskBox.value} `);
+  node.appendChild(textnode);
+  document.getElementById('tasks').appendChild(node);
+  let button = document.createElement("BUTTON");
+  button.innerText = "X";
+  node.appendChild(button);
+  event.preventDefault();
+}, false);
