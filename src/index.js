fe3e1806fd89ff1147902b634483bdb598f97a9b
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-23 19:04:48 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const todos = [];
+  const submit = document.querySelector(
+    "#create-task-form input[type='submit']"
+  );
+  const tasks = document.getElementById("tasks");
+  const input = document.getElementById("new-task-description");
+
+  submit.addEventListener("click", e => {
+    e.preventDefault();
+    const todo = input.value.trim();
+    // don't add blank todos
+    if (todo == "") {
+      return;
+    }
+
+    addTodo(todo);
+
+    // display todo
+    const li = document.createElement("li");
+
+    li.innerText = todo;
+    li.appendChild(createCloseButton(todo));
+    tasks.appendChild(li);
+
+    // clear input
+    todo = "";
+  });
+
+  function createCloseButton(todo) {
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.id = createID(todo);
+    return button;
+  }
+
+  function createID(todo) {
+    return todo.split(" ").join("-");
+  }
+
+  function addTodo(todo) {
+    todos.push(todo);
+  }
+
+  function deleteTodo(todo) {}
+
+  function findTodoIndex(todo) {}
+
+  function displayAllTodos() {
+    todos.forEach(todo => {
+      const li = document.createElement("li");
+      li.innerText = todo;
+      tasks.appendChild(li);
+    });
+  }
 });
