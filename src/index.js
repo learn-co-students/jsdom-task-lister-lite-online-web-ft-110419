4d4247f32ade026fbe1f9a186c8a6c8f7ab5183e
ncaudill27 <nelson.a.caudill@gmail.com>
2020-02-27 10:53:34 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  let listerForm = document.querySelector('form');
+  let searchBar = document.getElementById('new-task-description');
+  let tasks = document.getElementById('tasks');
+
+  function mySubmit(event) {
+    addTask(searchBar.value);
+    resetInput();
+    event.preventDefault();
+  }
+  
+  function addTask(task) {
+    let li = document.createElement('li');
+    if (task.length !== 0) {
+      li.textContent = task
+      tasks.appendChild(li)
+    };
+  }
+
+  function resetInput() {
+    searchBar.value = ""
+  }
+  listerForm.addEventListener('submit', mySubmit)
+  
 });
