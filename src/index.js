32db5ddb1d08437ce7e9236058d41b0a9982d910
Ethan <ethan_gustafson@outlook.com>
2020-02-27 21:50:27 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,22 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     let ul = document.getElementById("list");
-    let li = document.createElement("li");
     let taskForm = document.getElementById("create-task-form");
     let taskDescription = document.getElementById("new-task-description");
-    let taskList = document.getElementById("tasks")
-
+  
     taskForm.addEventListener("submit", function(event) { // listen for the submit button
+      let li = document.createElement("li");
+      li.id = "task"
+      let del = document.createElement("button")
+      del.innerText = "Delete";
       li.innerHTML = taskDescription.value; // new li inner HTML will be the new form input text field description value
       ul.appendChild(li); // append the li to the ul list
+      li.appendChild(del);
+
+        del.addEventListener("click", (event) => {
+          let grab = document.getElementById("task")
+          grab.remove();
+        });
       event.preventDefault(); // do not allow redirect, instead update the page
       // event.target.reset(); 
     });
@@ -22,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
 // Stretch Deliverables:
 // Try to implement one or more of the following:
 
-// A delete function that will remove tasks from your list
+// A delete function that will remove tasks from your list  => DONE ✅
 // A priority value selected from a dropdown that is used to determine the color of the text in the list (e.g. red for high priority, yellow for medium, green for low)
 // As a challenge, implement a sorting functionality that displays the tasks ascending or descending order based on priority
 // An additional input field (e.g. user, duration, date due)
